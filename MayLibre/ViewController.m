7990b7dd3f2e28a827bab94c8db76ea5fe70c85b
Jo Brunner <jo.brunner@mayflower.de>
2016-04-22 12:22:51 +0200

    Adds standard files from single view project.

---
+++ MayLibre/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MayLibre
+//
+//  Created by Jo Brunner on 22.04.16.
+//  Copyright © 2016 Mayflower. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MayLibreTests/MayLibreTests.m
@@ -0,0 +1,39 @@
+//
+//  MayLibreTests.m
+//  MayLibreTests
+//
+//  Created by Jo Brunner on 22.04.16.
+//  Copyright © 2016 Mayflower. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MayLibreTests : XCTestCase
+
+@end
+
+@implementation MayLibreTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
