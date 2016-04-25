88f159a67670677dd21062b3a52424e11cad3b82
Jo Brunner <jo.brunner@mayflower.de>
2016-04-26 00:27:14 +0200

    Adds a dummy range message from international isbn agency.

---
+++ MayLibre/MayISBNRangeDictionary.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+@interface MayISBNRangeDictionary : NSObject
+
++ (NSDictionary *)rangeDictionary;
+
+@end
---
+++ MayLibre/MayISBNRangeDictionary.m
@@ -0,0 +1,13 @@
+#import "MayISBNRangeDictionary.h"
+
+@implementation MayISBNRangeDictionary
+
+/**
+ * Dummy implementation
+ */
++ (NSDictionary *)rangeDictionary {
+    
+    return @{};
+}
+
+@end
