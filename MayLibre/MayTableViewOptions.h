1621b86ff87d73a77844bca84307f8b0789ac52e
Jo Brunner <jo.brunner@mayflower.de>
2016-09-04 15:29:58 +0200

    Adds reference key constants to Entry.plist

---
+++ MayLibre/MayTableViewOptions.h
@@ -0,0 +1,26 @@
+//
+//  MayTableViewOptions.h
+//  MayLibre
+//
+//  Created by Jo Brunner on 28.08.16.
+//  Copyright © 2016 Mayflower. All rights reserved.
+//
+
+#ifndef MayTableViewOptions_h
+#define MayTableViewOptions_h
+
+#define kMayTableViewOptionsBagItemIdKey                @"tag"
+#define kMayTableViewOptionsBagItemFieldKey             @"field"
+#define kMayTableViewOptionsBagItemAscendingKey         @"ascending"
+#define kMayTableViewOptionsBagItemTextKey              @"textkey"
+#define kMayTableViewOptionsBagItemVisibilityKey        @"visible"
+
+#define kMayTableViewOptionsBagSectionSortDefaultId     @"sortTag"
+#define kMayTableViewOptionsBagSectionFilterDefaultId   @"filterTag"
+
+#define kMayTableViewOptionsBagSectionSort              @"sort"
+#define kMayTableViewOptionsBagSectionFilter            @"filter"
+#define kMayTableViewOptionsBagSectionAction            @"action"
+#define kMayTableViewOptionsBagSectionDefaults          @"defaults"
+
+#endif /* MayTableViewOptions_h */
