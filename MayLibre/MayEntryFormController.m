ebf38791bda044f44e89ecaafc69e15e80323338
Jo Brunner <jo.brunner@mayflower.de>
2016-09-06 00:48:35 +0200

    Adds a first code for replacing or removing a user generated cover

--- MayLibre/MayEntryFormController.m
+++ MayLibre/MayEntryFormController.m
@@ -12,6 +12,7 @@
 #import "MayImageManager.h"
 #import "MayEntrySummaryFormCell.h"
 #import "MayUserDefaults.h"
+#import "MayDigest.h"
 
 @implementation MayEntryFormController
 
@@ -89,7 +90,43 @@ heightForFooterInSection:(NSInteger)section {
     _isbnTextField.text = _entry.productCode;
     _placeTextField.text = _entry.place;
     _summaryLabel.text = _entry.summary;
+    
+    NSLog(@"Alternative Cover: %@", _entry.userFilename);
+    
+    
+    // wenn userFilename einen Wert hat, soll zunächst ein thumbnail dazu geladen werden.
+    // Wenn das Thumbnail in diesem Cache nicht existiert, soll es erzeugt und geladen werden
+    // Das Original-Foto soll weiterhin im Dokumentenverzeichnis/Pictures liegen und später auf die
+    // iCloude synchronisiert werden. Die Thumbnails nicht. Auch nicht die Bilder aus dem Web (Copyrights
+    // und Speicher etc. - die gehören Google bzw. Amazon).
+    
+    if (_entry.userFilename != nil) {
+
+        // Zum Testen das Original Foto laden und anzeigen
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *path = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"Pictures"];
         
+        // create the full file path
+        NSString *filePath = [path stringByAppendingPathComponent:[NSString stringWithFormat:@"%@", _entry.userFilename]];
+
+        NSData *imageData = [NSData dataWithContentsOfFile:filePath];
+        NSLog(@"File: %@", filePath);
+        UIImage *image = [UIImage imageWithData:imageData];
+        _bookImage.image = image;
+        
+        return;
+        
+        
+        
+//        if (imageData.length > 0) {
+//            UIImage *image = [UIImage imageWithData:imageData];
+//            _bookImage.image = image;
+//
+//            return;
+//        }
+        
+    }
+    
     [[MayImageManager sharedManager] imageWithUrlString:_entry.coverUrl
                                              completion:^(UIImage *image, NSError *error) {
                                                  if (error) {
@@ -236,6 +273,191 @@ heightForFooterInSection:(NSInteger)section {
 - (IBAction)placeTextFieldChanged:(UITextField *)sender {
     
     [self formDidChanged:sender];
+}
+
+- (void)removeCurrentCover {
+    
+    UIAlertController *actionSheet =
+    [UIAlertController alertControllerWithTitle:nil // NSLocalizedString(@"", nil)
+                                        message:nil
+                                 preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *removeCoverAction =
+    [UIAlertAction actionWithTitle:NSLocalizedString(@"Remove Cover", nil)
+                             style:UIAlertActionStyleDestructive
+                           handler:^(UIAlertAction * action) {
+                               
+                               [[MayImageManager sharedManager] removeUserFile:_entry.userFilename
+                                                                    completion:^(NSError *error) {
+                                                                    
+                                                                        if (error == nil) {
+                                                                            _entry.userFilename = nil;
+                                                                            [managedObjectContext save:&error];
+                                                                        }
+                                                                        
+                                                                        if (error) {
+                                                                            [App viewController:self
+                                                                                handleUserError:error
+                                                                                          title:nil];
+                                                                        }
+                                                                        
+                                                                    }];
+                           }];
+    
+    UIAlertAction *cancelAction =
+    [UIAlertAction actionWithTitle:NSLocalizedString(@"Cancel", nil)
+                             style:UIAlertActionStyleCancel
+                           handler:nil];
+    
+    [actionSheet addAction:removeCoverAction];
+    [actionSheet addAction:cancelAction];
+    
+    [self presentViewController:actionSheet
+                       animated:YES
+                     completion:nil];
+    
+}
+
+- (void)replaceCurrentCover {
+    
+    UIAlertController *actionSheet =
+    [UIAlertController alertControllerWithTitle:NSLocalizedString(@"Replace Cover", nil)
+                                        message:nil
+                                 preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *lastPhotoTakenAction =
+    [UIAlertAction actionWithTitle:NSLocalizedString(@"Last Photo Taken", nil)
+                             style:UIAlertActionStyleDefault
+                           handler:^(UIAlertAction * action) {
+
+                               NSLog(@"Last Photo Taken aus Gallary holen, speichern und UI refresh");
+                               
+                           }];
+    
+    UIAlertAction *takePhotoAction =
+    [UIAlertAction actionWithTitle:NSLocalizedString(@"Take Photo", nil)
+                             style:UIAlertActionStyleDefault
+                           handler:^(UIAlertAction * action) {
+
+                               NSLog(@"Take Photo, speichern und UI refresh");
+
+                           }];
+    
+    UIAlertAction *chooseFromLibraryAction =
+    [UIAlertAction actionWithTitle:NSLocalizedString(@"Choose from Library", nil)
+                             style:UIAlertActionStyleDefault
+                           handler:^(UIAlertAction * action) {
+
+                               [self takeCoverFromLibrary];
+                               
+                               NSLog(@"Choose from Library, speichern und UI refresh");
+                           
+                           }];
+    
+    UIAlertAction *cancelAction =
+    [UIAlertAction actionWithTitle:NSLocalizedString(@"Cancel", nil)
+                             style:UIAlertActionStyleCancel
+                           handler:nil];
+    
+    [actionSheet addAction:lastPhotoTakenAction];
+    [actionSheet addAction:takePhotoAction];
+    [actionSheet addAction:chooseFromLibraryAction];
+    [actionSheet addAction:cancelAction];
+    
+    [self presentViewController:actionSheet
+                       animated:YES
+                     completion:nil];
+    
+}
+
+- (IBAction)changeCoverImage:(UIButton *)sender {
+    
+// Wenn noch kein Cover vorhanden ist:
+    // Last Photo Taken
+    // Take Photo
+    // Choose from Library
+    // ---
+    // Cancel
+    
+// Wenn bereits ein Cover vorhanden ist:
+    // Remove Photo
+    // ---
+    // Cancel
+    
+    if (_entry.coverUrl != nil || _entry.userFilename != nil) {
+        
+        // remove photo
+        [self removeCurrentCover];
+    }
+    else {
+        [self replaceCurrentCover];
+    }
+}
+
+- (void)takeCoverFromLibrary {
+    
+    UIImagePickerController *picker = [UIImagePickerController new];
+    
+    picker.delegate = self;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+
+    [self presentViewController:picker
+                       animated:YES
+                     completion:NULL];
+}
+
+- (void)takePhoto {
+    
+    UIImagePickerController *picker = [UIImagePickerController new];
+
+    picker.delegate = self;
+
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        picker.allowsEditing = YES;
+        picker.showsCameraControls = YES;
+    }
+    else {
+        picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+
+    [self presentViewController:picker
+                       animated:YES
+                     completion:NULL];
+}
+
+#pragma mark - UIImagePickerController Delegates -
+
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    
+    [[MayImageManager sharedManager] storeImage:chosenImage
+                                     completion:^(NSString *filename, NSError *error) {
+                                         
+                                         NSLog(@"filename: %@", filename);
+                                         
+                                         if (error == nil) {
+                                             _entry.userFilename = filename;
+                                             [managedObjectContext save:&error];
+                                         }
+                                         if (error) {
+                                             [App viewController:self
+                                                 handleUserError:error
+                                                           title:nil];
+                                         }
+                                         
+                                     }];
+
+    [picker dismissViewControllerAnimated:YES
+                               completion:NULL];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [picker dismissViewControllerAnimated:YES
+                               completion:NULL];
 }
 
 #pragma mark - Segues
