9efd52193aa97f3369518de0330b0a9cfc7fa8af
Jo Brunner <jo.brunner@mayflower.de>
2017-04-24 01:26:02 +0200

    Adds authorization handling, speeds up upcomming of camera and refactoring a little bit.

--- MayLibre/MayBarCodeScannerController.m
+++ MayLibre/MayBarCodeScannerController.m
@@ -24,6 +24,9 @@
 @property (nonatomic, strong) UIView *consoleView;
 @property (nonatomic, strong) UIButton *cancelButton;
 @property (nonatomic, strong) id subjectAreaDidChangeObserver;
+@property (nonatomic) dispatch_queue_t sessionQueue;
+@property (nonatomic) MayBarCodeScannerSetupResult setupResult;
+
 
 - (IBAction)tapGestureRecognizer:(UITapGestureRecognizer *)sender;
 
@@ -35,20 +38,116 @@
 
 - (void)viewDidLoad {
 
-    [self.navigationController setToolbarHidden:YES
-                                       animated:NO];
     [super viewDidLoad];
 
-    NSError *error = nil;
-    [self initScanner:&error];
+    [self.navigationController setToolbarHidden:YES
+                                       animated:YES];
+    // Create Session Queue
+    self.sessionQueue = dispatch_queue_create("session queue", DISPATCH_QUEUE_SERIAL);
+
+    // Create the AVCaptureSession
+    self.session = [[AVCaptureSession alloc] init];
+
+    [self initPreviewLayer];
     
-    if (error != nil) {
-        // handle Error
-        NSLog(@"%@", error.userInfo);
-    }
-    else {
-        [self startScanning];
-    }
+    // Handle Authorization
+    [self authorizeCamara];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+
+    [super viewWillAppear:animated];
+    
+    dispatch_async(self.sessionQueue, ^{
+
+        switch (self.setupResult) {
+                
+            case MayBarCodeScannerSetupResultSuccess:
+            {
+                [self startScanning];
+                break;
+            }
+
+            case MayBarCodeScannerSetupResultCameraNotAuthorized:
+            {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    
+                    NSString *message
+                    = NSLocalizedString(@"MayLibre doesn't have permission to use the camera, please change privacy settings",
+                                        @"Alert message when the user has denied access to the camera");
+                    
+                    UIAlertController *alertController
+                    = [UIAlertController alertControllerWithTitle:@"MayLibre"
+                                                          message:message
+                                                   preferredStyle:UIAlertControllerStyleAlert];
+                    
+                    UIAlertAction *cancelAction
+                    = [UIAlertAction actionWithTitle:NSLocalizedString(@"OK",
+                                                                       @"Alert OK button")
+                                               style:UIAlertActionStyleCancel
+                                             handler:nil];
+                    
+                    [alertController addAction:cancelAction];
+                    
+                    // Provide quick access to Settings
+                    UIAlertAction *settingsAction
+                    = [UIAlertAction actionWithTitle:NSLocalizedString(@"Settings",
+                                                                       @"Alert button to open Settings")
+                                               style:UIAlertActionStyleDefault
+                                             handler:^( UIAlertAction *action ) {
+                                                 
+                                                 [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString] options:@{} completionHandler:nil];
+                                             }];
+                    
+                    [alertController addAction:settingsAction];
+                    
+                    [self presentViewController:alertController
+                                       animated:YES
+                                     completion:nil];
+                });
+                break;
+            }
+                
+            case MayBarCodeScannerSetupResultSessionConfigurationFailed:
+            {
+                dispatch_async(dispatch_get_main_queue(), ^{
+            
+                    NSString *message
+                    = NSLocalizedString(@"Unable to capture bar codes",
+                                        @"Alert message when something goes wrong during capture session configuration");
+                    
+                    UIAlertController *alertController
+                    = [UIAlertController alertControllerWithTitle:@"MayLibre"
+                                                          message:message
+                                                   preferredStyle:UIAlertControllerStyleAlert];
+                    
+                    UIAlertAction *cancelAction
+                    = [UIAlertAction actionWithTitle:NSLocalizedString(@"OK",
+                                                                       @"Alert OK button")
+                                               style:UIAlertActionStyleCancel
+                                             handler:nil];
+                    
+                    [alertController addAction:cancelAction];
+                    
+                    [self presentViewController:alertController
+                                       animated:YES
+                                     completion:nil];
+                });
+                break;
+            }
+        }
+    });
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    
+    dispatch_async(self.sessionQueue, ^{
+        if (self.setupResult == MayBarCodeScannerSetupResultSuccess) {
+            [self stopScanning];
+        }
+    });
+    
+    [super viewDidDisappear:animated];
 }
 
 - (BOOL)prefersStatusBarHidden {
@@ -61,56 +160,140 @@
     [super didReceiveMemoryWarning];
 }
 
-#pragma mark Helper
+#pragma mark Authorization
 
-- (void)initScanner:(NSError **)error {
-    
-    // create a capture session
-    _session = [AVCaptureSession new];
-    
-    // create an video capture device
-    _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+- (void)authorizeCamara {
 
-    // restrict default auto focus to near scanning.
-    // User can tap a focus point to remove this behaviour and can focus by tap until scene changes.
-    if (_device.autoFocusRangeRestrictionSupported) {
-        if ([_device lockForConfiguration:nil]) {
-            _device.autoFocusRangeRestriction = AVCaptureAutoFocusRangeRestrictionNear;
-            [_device unlockForConfiguration];
+    self.setupResult = MayBarCodeScannerSetupResultSuccess;
+
+    switch ([AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo]) {
+        
+            // The user has previously granted access to the camera
+        case AVAuthorizationStatusAuthorized:
+        {
+            break;
+        }
+
+        // The user has not yet been presented with the option to grant video access.
+        // We suspend the session queue to delay session running until the access request has completed.
+        case AVAuthorizationStatusNotDetermined:
+        {
+            dispatch_suspend(self.sessionQueue);
+            [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo
+                                     completionHandler:^(BOOL granted) {
+                                         if (!granted) {
+                                             self.setupResult = MayBarCodeScannerSetupResultCameraNotAuthorized;
+                                         }
+                                         dispatch_resume( self.sessionQueue );
+                                     }];
+            break;
+        }
+            
+        default:
+        {
+            // The user has previously denied access
+            self.setupResult = MayBarCodeScannerSetupResultCameraNotAuthorized;
+            break;
         }
     }
     
-    // create an input capture device from the video capture device
-    _deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:_device
-                                                         error:error];
-    if (_deviceInput == nil) {
+    dispatch_async(self.sessionQueue, ^{
+        [self configureSession];
+    });
+}
 
+// Should be called on the session queue
+- (void)configureSession {
+
+    if (self.setupResult != MayBarCodeScannerSetupResultSuccess) {
+    
         return;
     }
 
-    // assign created inputed device to the av session
-    [_session addInput:_deviceInput];
+    [self.session beginConfiguration];
+    
+    // Create a default video device
+    AVCaptureDevice *videoDevice
+    = [AVCaptureDevice defaultDeviceWithDeviceType:AVCaptureDeviceTypeBuiltInWideAngleCamera
+                                         mediaType:AVMediaTypeVideo
+                                          position:AVCaptureDevicePositionUnspecified];
+    NSError *error = nil;
 
-    // configure session with metadata output
-    _metadataOutput = [AVCaptureMetadataOutput new];
-    [_metadataOutput setMetadataObjectsDelegate:self
-                                          queue:dispatch_get_main_queue()];
-    [_session addOutput:_metadataOutput];
+    // Create a input device
+    AVCaptureDeviceInput *videoDeviceInput = [AVCaptureDeviceInput deviceInputWithDevice:videoDevice
+                                                                                   error:&error];
+    if (error != nil) {
+        NSLog( @"Could not create video device input: %@", error );
+        self.setupResult = MayBarCodeScannerSetupResultSessionConfigurationFailed;
+        [self.session commitConfiguration];
+        
+        return;
+    }
 
-    if ([_metadataOutput.availableMetadataObjectTypes containsObject:AVMetadataObjectTypeEAN13Code]) {
-        _metadataOutput.metadataObjectTypes = @[AVMetadataObjectTypeEAN13Code];
+    
+    if ([self.session canAddInput:videoDeviceInput]) {
+    
+        // Adds input device to video session
+        [self.session addInput:videoDeviceInput];
+
+        if (self.device.autoFocusRangeRestrictionSupported) {
+            if ([self.device lockForConfiguration:nil]) {
+                self.device.autoFocusRangeRestriction = AVCaptureAutoFocusRangeRestrictionNear;
+                [self.device unlockForConfiguration];
+            }
+        }
+        
+        self.deviceInput = videoDeviceInput;
+        self.device = videoDevice;
+        
+        /*
+         Why are we dispatching this to the main queue?
+         Because AVCaptureVideoPreviewLayer is the backing layer for AVCamManualPreviewView and UIView
+         can only be manipulated on the main thread.
+         Note: As an exception to the above rule, it is not necessary to serialize video orientation changes
+         on the AVCaptureVideoPreviewLayer’s connection with other session manipulation.
+         
+         Use the status bar orientation as the initial video orientation. Subsequent orientation changes are
+         handled by -[AVCamManualCameraViewController viewWillTransitionToSize:withTransitionCoordinator:].
+         */
+        dispatch_async(dispatch_get_main_queue(), ^{
+
+            UIInterfaceOrientation statusBarOrientation = [UIApplication sharedApplication].statusBarOrientation;
+            AVCaptureVideoOrientation initialVideoOrientation = AVCaptureVideoOrientationPortrait;
+
+            // Orientation is relevant for iPads where supported
+            if (statusBarOrientation != UIInterfaceOrientationUnknown) {
+                initialVideoOrientation = (AVCaptureVideoOrientation)statusBarOrientation;
+            }
+            
+            AVCaptureVideoPreviewLayer *previewLayer = (AVCaptureVideoPreviewLayer *)self.previewLayer;
+            previewLayer.connection.videoOrientation = initialVideoOrientation;
+        });
+        
+        // configure session with metadata output
+        _metadataOutput = [AVCaptureMetadataOutput new];
+        [_metadataOutput setMetadataObjectsDelegate:self
+                                              queue:dispatch_get_main_queue()];
+        [_session addOutput:_metadataOutput];
+        
+        if ([_metadataOutput.availableMetadataObjectTypes containsObject:AVMetadataObjectTypeEAN13Code]) {
+            _metadataOutput.metadataObjectTypes = @[AVMetadataObjectTypeEAN13Code];
+        }
     }
     else {
-        NSDictionary * userInfo
-        = @{NSLocalizedDescriptionKey:NSLocalizedString(@"Your iOS version dosn't support EAN13 codes.", nil)};
-        
-        *error = [NSError errorWithDomain:kMayBarCodeScannerErrorDomain
-                                     code:MayBarCodeScannerBarCodeType
-                                 userInfo:userInfo];
+        NSLog( @"Could not add video device input to the session" );
+        self.setupResult = MayBarCodeScannerSetupResultSessionConfigurationFailed;
+        [self.session commitConfiguration];
+
+        return;
     }
     
-    [self initPreviewLayer];
+    [self.session commitConfiguration];
 }
+
+
+#pragma mark Helper
+
 
 - (void)initPreviewLayer {
     
@@ -140,13 +323,13 @@
     self.cancelButton = [UIButton buttonWithType:UIButtonTypeCustom];
     self.cancelButton.translatesAutoresizingMaskIntoConstraints = NO;
     self.cancelButton.contentMode = UIViewContentModeScaleAspectFit;
-    [self.cancelButton setTitle:NSLocalizedString(@"Cancel", nil)
+    [self.cancelButton setTitle:NSLocalizedString(@"Cancel", @"Camera Cancel button")
                        forState:UIControlStateNormal];
     [self.cancelButton setTitleColor:UIColor.whiteColor
                             forState:UIControlStateNormal];
     [self.cancelButton addTarget:self
                           action:@selector(didCancelTouchUpInside:)
-            forControlEvents:UIControlEventTouchUpInside];
+                forControlEvents:UIControlEventTouchUpInside];
     [self.consoleView addSubview:self.cancelButton];
     [self.view addSubview:self.consoleView];
     [self.view bringSubviewToFront:self.highlightView];
@@ -174,7 +357,15 @@
 - (void)startScanning {
     
     self.highlightView.frame = CGRectZero;
-    [_session startRunning];
+
+    if (self.device.autoFocusRangeRestrictionSupported) {
+        if ([self.device lockForConfiguration:nil]) {
+            self.device.autoFocusRangeRestriction = AVCaptureAutoFocusRangeRestrictionNear;
+            [self.device unlockForConfiguration];
+        }
+    }
+    
+    [self.session startRunning];
 
     if ([self.delegate respondsToSelector:@selector(barCodeScannerControllerDidStartScanning:)]) {
         [self.delegate barCodeScannerControllerDidStartScanning:self];
@@ -183,7 +374,7 @@
 
 - (void)stopScanning {
     
-    [_session stopRunning];
+    [self.session stopRunning];
 
     if ([self.delegate respondsToSelector:@selector(barCodeScannerControllerDidStopScanning:)]) {
         [self.delegate barCodeScannerControllerDidStopScanning:self];
@@ -207,7 +398,7 @@ didOutputMetadataObjects:(NSArray *)metadataObjects
 
                 barCodeObject =
                 (AVMetadataMachineReadableCodeObject *)
-                [_previewLayer transformedMetadataObjectForMetadataObject:(AVMetadataMachineReadableCodeObject *)metadata];
+                [self.previewLayer transformedMetadataObjectForMetadataObject:(AVMetadataMachineReadableCodeObject *)metadata];
                 
                 highlightViewRect = barCodeObject.bounds;
                 detectionString = [(AVMetadataMachineReadableCodeObject *)metadata stringValue];
@@ -253,17 +444,17 @@ didOutputMetadataObjects:(NSArray *)metadataObjects
     };
     
     UIAlertAction *applyAction =
-    [UIAlertAction actionWithTitle:NSLocalizedString(@"Apply", nil)
+    [UIAlertAction actionWithTitle:NSLocalizedString(@"Apply", @"Barcode Apply action")
                              style:UIAlertActionStyleDefault
                            handler:applyActionHandler];
 
     UIAlertAction *retryAction =
-    [UIAlertAction actionWithTitle:NSLocalizedString(@"Retry", nil)
+    [UIAlertAction actionWithTitle:NSLocalizedString(@"Retry", @"Barcode Retry action")
                              style:UIAlertActionStyleDefault
                            handler:retryActionHandler];
     
     UIAlertAction *cancelAction =
-    [UIAlertAction actionWithTitle:NSLocalizedString(@"Cancel", nil)
+    [UIAlertAction actionWithTitle:NSLocalizedString(@"Cancel", @"Barcode Cancel action")
                              style:UIAlertActionStyleDestructive
                            handler:cancelActionHandler];
     
@@ -274,7 +465,9 @@ didOutputMetadataObjects:(NSArray *)metadataObjects
         NSLog(@"%@", error.localizedDescription);
         
         // Action Sheet negative
-        actionSheet = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"No valid ISBN", nil)
+        actionSheet = [UIAlertController alertControllerWithTitle:NSLocalizedString(
+                                                                                    @"No valid ISBN",
+                                                                                    @"Alert message when scanned ISBN is not valid")
                                                           message:barCode
                                                    preferredStyle:UIAlertControllerStyleActionSheet];
         [actionSheet addAction:retryAction];
@@ -282,7 +475,8 @@ didOutputMetadataObjects:(NSArray *)metadataObjects
     }
     else {
         // Action Sheet positive
-        actionSheet = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"ISBN Found", nil)
+        actionSheet = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"ISBN Found",
+                                                                                    @"Alert message when scanned ISBN is valid")
                                                           message:[MayISBNFormatter stringFromISBN:isbn]
                                                    preferredStyle:UIAlertControllerStyleActionSheet];
         [actionSheet addAction:applyAction];
@@ -295,7 +489,9 @@ didOutputMetadataObjects:(NSArray *)metadataObjects
                      completion:nil];
 }
 
+
 #pragma mark Actions
+
 
 - (void)didCancelTouchUpInside:(id)sender {
     
@@ -313,7 +509,6 @@ didOutputMetadataObjects:(NSArray *)metadataObjects
          exposeWithMode:AVCaptureExposureModeAutoExpose
           atDevicePoint:devicePoint
       subjectAreaChange:YES];
-    
 }
 
 - (void)focusWithMode:(AVCaptureFocusMode)focusMode
